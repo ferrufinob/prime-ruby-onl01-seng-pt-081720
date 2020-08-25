e98dd1c9497009a70aebbb0ba07fd11f967f02f0
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-25 17:45:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,6 @@
 # Add  code here!
 
 def prime?(integer)
-  # return false if integer < 2
-  # (2..integer/2).none?{|i| integer % i == 0}
   return false if integer < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
-            return false
-        end
-    end
-    true
+  (2..integer/2).none?{|i| integer % i == 0}
 end
